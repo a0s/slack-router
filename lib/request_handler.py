898481ad43c76c3e6121d3300a1947d9ba3d59a7
Anton Osenenko <anton@beyondplay.io>
2023-03-08 17:56:02 +0100

    Debug request payload

--- lib/request_handler.py
+++ lib/request_handler.py
@@ -1,3 +1,4 @@
+import logging
 import re
 import urllib
 from http.server import BaseHTTPRequestHandler
@@ -23,7 +24,11 @@ def request_handler(config_parser: ConfigParser) -> type[BaseHTTPRequestHandler]
             content_len = int(self.headers['Content-Length'])
             body = self.rfile.read(content_len).decode('utf-8')
             query = urllib.parse.parse_qs(body)
-            raw_payload = query['payload'][0]
+            logging.debug(f"Request query: {query['payload']}")
+            if type(query['payload']) is list:
+                raw_payload = query['payload'][0]
+            else:
+                raw_payload = query['payload']
             json = simplejson.loads(raw_payload)
 
             route = None
