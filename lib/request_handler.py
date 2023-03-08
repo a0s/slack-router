d5260a7915c3be25990282a3cda390a5c7cbce58
Anton Osenenko <anton@beyondplay.io>
2023-03-08 20:00:15 +0100

    remove payload processing

--- lib/request_handler.py
+++ lib/request_handler.py
@@ -1,11 +1,8 @@
-import logging
 import re
-import urllib
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse
 
 import requests
-import simplejson
 
 from lib.config_parser import ConfigParser
 
@@ -23,25 +20,53 @@ def request_handler(config_parser: ConfigParser) -> type[BaseHTTPRequestHandler]
         def do_POST(self):
             content_len = int(self.headers['Content-Length'])
             body = self.rfile.read(content_len).decode('utf-8')
-            query = urllib.parse.parse_qs(body)
-            logging.debug(f"Request query: {query['payload']}")
-            if type(query['payload']) is list:
-                raw_payload = query['payload'][0]
-            else:
-                raw_payload = query['payload']
-            json = simplejson.loads(raw_payload)
 
             route = None
             for route in config_parser.parsed['routes']:
-                if re.fullmatch(route['body_re'], json['text']):
+                if re.fullmatch(route['body_re'], body):
                     break
-
             if route is None:
                 self.send_response(418)
                 return
 
-            if 'channel' in route:
-                json['channel'] = route['channel']
+            #
+            # json = None
+            # try:
+            #     json = simplejson.loads(body)
+            #
+            # except:
+            #     pass
+            #
+            # try:
+            #     query = urllib.parse.parse_qs(body)
+            #     if type(query['payload']) is list:
+            #         raw_payload = query['payload'][0]
+            #     else:
+            #         raw_payload = query['payload']
+            #     json = simplejson.loads(raw_payload)
+            # except:
+            #     pass
+            #
+            # if json is None:
+            #     self.send_response(418)
+            #     return
+
+            # route = None
+            # for route in config_parser.parsed['routes']:
+            #     if 'text' in json:
+            #         if re.fullmatch(route['body_re'], json['text']):
+            #             break
+            #     else:
+            #         if re.fullmatch(route['body_re'], body):
+            #             break
+            #
+            # if route is None:
+            #     self.send_response(418)
+            #     return
+            #
+            # if 'channel' in route:
+            #     json['channel'] = route['channel']
+            # request = requests.post(route['url'], headers=headers, data={'payload': simplejson.dumps(json)})
 
             headers = {}
             for h in self.headers:
@@ -50,7 +75,7 @@ def request_handler(config_parser: ConfigParser) -> type[BaseHTTPRequestHandler]
                 else:
                     headers[h] = self.headers[h]
 
-            request = requests.post(route['url'], headers=headers, data={'payload': simplejson.dumps(json)})
+            request = requests.post(route['url'], headers=headers, data=body)
             self.send_response(request.status_code)
             for k, v in request.headers.items():
                 self.send_header(k, v)
