93c82c00952b51da19a1759797d47fb69aaa5e73
Anton Osenenko <anton@beyondplay.io>
2023-03-11 12:54:47 +0100

    Fix regexp decoder

--- lib/request_handler.py
+++ lib/request_handler.py
@@ -23,50 +23,12 @@ def request_handler(config_parser: ConfigParser) -> type[BaseHTTPRequestHandler]
 
             route = None
             for route in config_parser.parsed['routes']:
-                if re.fullmatch(route['body_re'], body):
+                m = re.search(route['body_re'], body)
+                if m:
                     break
             if route is None:
                 self.send_response(418)
                 return
-
-            #
-            # json = None
-            # try:
-            #     json = simplejson.loads(body)
-            #
-            # except:
-            #     pass
-            #
-            # try:
-            #     query = urllib.parse.parse_qs(body)
-            #     if type(query['payload']) is list:
-            #         raw_payload = query['payload'][0]
-            #     else:
-            #         raw_payload = query['payload']
-            #     json = simplejson.loads(raw_payload)
-            # except:
-            #     pass
-            #
-            # if json is None:
-            #     self.send_response(418)
-            #     return
-
-            # route = None
-            # for route in config_parser.parsed['routes']:
-            #     if 'text' in json:
-            #         if re.fullmatch(route['body_re'], json['text']):
-            #             break
-            #     else:
-            #         if re.fullmatch(route['body_re'], body):
-            #             break
-            #
-            # if route is None:
-            #     self.send_response(418)
-            #     return
-            #
-            # if 'channel' in route:
-            #     json['channel'] = route['channel']
-            # request = requests.post(route['url'], headers=headers, data={'payload': simplejson.dumps(json)})
 
             headers = {}
             for h in self.headers:
@@ -75,11 +37,13 @@ def request_handler(config_parser: ConfigParser) -> type[BaseHTTPRequestHandler]
                 else:
                     headers[h] = self.headers[h]
 
-            request = requests.post(route['url'], headers=headers, data=body)
-            self.send_response(request.status_code)
-            for k, v in request.headers.items():
+            response = requests.post(route['url'], headers=headers, data=body, stream=True)
+            response_raw = response.raw.read()
+
+            self.send_response(response.status_code)
+            for k, v in response.headers.items():
                 self.send_header(k, v)
             self.end_headers()
-            self.wfile.write(request.content)
+            self.wfile.write(response_raw)
 
     return Handler
